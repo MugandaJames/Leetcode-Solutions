b35eecd5b94c1bf76f04cacc2d307b9f6ac209c3
Muganda James Juma <mugandajames403@gmail.com>
2026-02-12 16:56:28 +0300

    Implementation of divide function for two integers

---
+++ divideTwoIntegers/divideTwoIntegers.py
@@ -0,0 +1,29 @@
+class Solution:
+    def divide(self, dividend, divisor):
+        # Handle overflow
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+
+        # Special case
+        if dividend == INT_MIN and divisor == -1:
+            return INT_MAX
+
+        # Determine sign
+        negative = (dividend < 0) != (divisor < 0)
+
+        # Work with positive numbers
+        dividend, divisor = abs(dividend), abs(divisor)
+        quotient = 0
+
+        while dividend >= divisor:
+            temp, multiple = divisor, 1
+            # Double temp until it is bigger than dividend
+            while dividend >= temp << 1:
+                temp <<= 1
+                multiple <<= 1
+
+            # Subtract largest multiple
+            dividend -= temp
+            quotient += multiple
+
+        return -quotient if negative else quotient
