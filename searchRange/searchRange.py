f163717656146fe439ca858eec10d0c5a28a53b5
Muganda James Juma <mugandajames403@gmail.com>
2026-03-24 22:58:41 +0300

    Implement searchRange to find target indices

---
+++ searchRange/searchRange.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def searchRange(self, nums, target):
+        
+        def findLeft():
+            left, right = 0, len(nums) - 1
+            res = -1
+            
+            while left <= right:
+                mid = (left + right) // 2
+                
+                if nums[mid] == target:
+                    res = mid
+                    right = mid - 1  # keep searching LEFT
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            
+            return res
+        
+        def findRight():
+            left, right = 0, len(nums) - 1
+            res = -1
+            
+            while left <= right:
+                mid = (left + right) // 2
+                
+                if nums[mid] == target:
+                    res = mid
+                    left = mid + 1  # keep searching RIGHT
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            
+            return res
+        
+        return [findLeft(), findRight()]
