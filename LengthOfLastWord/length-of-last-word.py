1dd8c8ce9c440a2fc276ee5a6fafc8289171054a
Muganda James Juma <mugandajames403@gmail.com>
2026-05-09 23:57:32 +0300

    Create length-of-last-word.py

---
+++ LengthOfLastWord/length-of-last-word.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        i = len(s) - 1
+        length = 0
+
+        # skip trailing spaces
+        while i >= 0 and s[i] == " ":
+            i -= 1
+
+        # count last word
+        while i >= 0 and s[i] != " ":
+            length += 1
+            i -= 1
+
+        return length
