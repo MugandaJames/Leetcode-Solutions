1489f96b6c5979075e292deb1f10f2437ab67c29
Muganda James Juma <mugandajames403@gmail.com>
2026-05-10 13:43:40 +0300

    Implementation of combination generation algorithm

---
+++ Combinations/combinations.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+
+        result = []
+        path = []
+
+        def backtrack(start):
+
+            # base case
+            if len(path) == k:
+                result.append(path[:])
+                return
+
+            for i in range(start, n + 1):
+
+                path.append(i)
+
+                backtrack(i + 1)
+
+                path.pop()
+
+        backtrack(1)
+
+        return result
