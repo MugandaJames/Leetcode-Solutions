556a8319312eb7905481c5ffdb2215419c02b47d
Muganda James Juma <mugandajames403@gmail.com>
2026-05-11 20:51:52 +0300

    Create pascals-triangle2.py

---
+++ Pascal'sTriangle2/pascals-triangle2.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+
+        row = [1] * (rowIndex + 1)
+
+        for i in range(2, rowIndex + 1):
+
+            # update from right to left
+            for j in range(i - 1, 0, -1):
+                row[j] = row[j] + row[j - 1]
+
+        return row
