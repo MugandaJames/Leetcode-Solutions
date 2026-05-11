382bdb94cbd4a456624ce679ffacfbf22eb16bb4
Muganda James Juma <mugandajames403@gmail.com>
2026-05-11 20:52:35 +0300

    Create triangle.py

---
+++ Triangle/triangle.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+
+        dp = triangle[-1][:]  # start from last row
+
+        for i in range(len(triangle) - 2, -1, -1):
+
+            for j in range(len(triangle[i])):
+
+                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1])
+
+        return dp[0]
