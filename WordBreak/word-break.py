e4675ed14158c624ecb59b902d785e97c0c27e3b
Muganda James Juma <mugandajames403@gmail.com>
2026-05-12 19:05:20 +0300

    Create word-break.py

---
+++ WordBreak/word-break.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+
+        wordSet = set(wordDict)
+        n = len(s)
+
+        dp = [False] * (n + 1)
+
+        # Empty string is valid
+        dp[0] = True
+
+        for i in range(1, n + 1):
+
+            for j in range(i):
+
+                if dp[j] and s[j:i] in wordSet:
+                    dp[i] = True
+                    break
+
+        return dp[n]
