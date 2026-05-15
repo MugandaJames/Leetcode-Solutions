ff970b28be8278d676ef9b5f15c3632505729523
Muganda James Juma <mugandajames403@gmail.com>
2026-05-15 21:35:18 +0300

    Implement version comparison in compare-version-numbers.py

---
+++ CompareVersionNumbers/compare-version-numbers.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def compareVersion(self, version1, version2):
+        v1 = list(map(int, version1.split('.')))
+        v2 = list(map(int, version2.split('.')))
+
+        n = max(len(v1), len(v2))
+
+        for i in range(n):
+            a = v1[i] if i < len(v1) else 0
+            b = v2[i] if i < len(v2) else 0
+
+            if a > b:
+                return 1
+            elif a < b:
+                return -1
+
+        return 0
